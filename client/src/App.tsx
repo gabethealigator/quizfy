c5ce12225313695f50602ac112d435f866606092
gabrielmslima <gabrielnunesinfor@gmail.com>
2025-02-12 20:53:26 -0300

    refactor: Wrap App with BrowserRouter and use navigate for routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,38 @@
-import { useEffect } from 'react'
-import './App.css'
-import Router from './routes/Router';
-import { AuthProvider, useAuth } from './context/AuthContext';
+import { useEffect } from "react";
+import "./App.css";
+import Router from "./routes/Router";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import { BrowserRouter, useNavigate } from "react-router-dom";
 
 function AuthHandler() {
   const { login } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const hash = window.location.hash;
     if (hash) {
       const params = new URLSearchParams(hash.substring(1));
-      const token = params.get('access_token');
+      const token = params.get("access_token");
       if (token) {
         login(token);
-        window.location.hash = '';
-        window.location.href = '/home';
+        window.location.hash = "";
+        navigate("/home");
       }
     }
-  }, [login]);
+  }, [login, navigate]);
 
   return null;
 }
 
 function App() {
   return (
-    <AuthProvider>
-      <AuthHandler />
-      <Router />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <AuthHandler />
+        <Router />
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
-export default App
-
+export default App;
