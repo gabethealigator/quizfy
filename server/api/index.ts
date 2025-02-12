370b6a96abe491e0b600f430888078a9a85b949d
gabrielmslima <gabrielnunesinfor@gmail.com>
2025-02-12 17:20:54 -0300

    chore: changed rewrite api path

--- server/api/index.ts
+++ server/api/index.ts
@@ -7,9 +7,9 @@ export default function handler(req: IncomingMessage, res: ServerResponse) {
   if (!req.url) {
     req.url = '/'
   }
-  
+
   return new Promise((resolve) => {
     server.emit('request', req, res)
     res.on('finish', resolve)
   })
-} 
+}
