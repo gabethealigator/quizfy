203c50343895168ba9b4337224e7ad2666a917a0
gabrielmslima <gabrielnunesinfor@gmail.com>
2025-02-12 20:41:55 -0300

    bro: bro

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,34 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(() => {
-  return {
-    plugins: [
-      react(),
-      tailwindcss()
-    ],
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://tracktryvia-api.vercel.app",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
+export default defineConfig(() => ({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://tracktryvia-api.vercel.app",
+        changeOrigin: true
       }
-    },
-    preview: {
-      proxy: {
-        "/api": {
-          target: "https://tracktryvia-api.vercel.app",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: true
     }
+  },
+  preview: {
+    proxy: {
+      "/api": {
+        target: "https://tracktryvia-api.vercel.app",
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
-})
+}))
 
